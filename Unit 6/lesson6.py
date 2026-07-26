cce383893647e404e2b833f4e6ad9d3b67ad71e8
Nqobile Twala <nqobiletwala02@gmail.com>
2026-07-26 19:43:56 +0200

    Lesson 6: working with lists and dictionaries

---
+++ Unit 6/lesson6.py
@@ -0,0 +1,22 @@
+# Working with lists (shopping cart)
+
+# cart = ["Apples", "Banana", "Berries"]
+# print(cart[0])
+
+# cart.append("watermelon")
+# print(cart)
+
+# for item in cart:
+#     print(item)
+
+
+# Dictionaries
+
+student = {
+    "name": "Liam",
+    "age": 22,
+    "course": "Data Science"
+}
+
+print(student["name"])
+print(student["age"])
